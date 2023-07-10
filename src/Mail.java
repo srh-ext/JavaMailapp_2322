15a7e2f3282f25c10c14fd35e3e0506560581c63
Yuriy S <dev@solovyov.de>
2023-07-10 13:26:41 +0200

    add libs

---
+++ src/Mail.java
@@ -0,0 +1,42 @@
+public class Mail {
+
+    private String sender;
+    private String recipient;
+    private String subject;
+    private String message;
+
+    public Mail() {
+    }
+
+    public String getSender() {
+        return this.sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return this.recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.sender = sender;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/SMTP.java
@@ -0,0 +1,11 @@
+public class SMTP {
+
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+    private static final String HOST = "";
+    private static final String PORT = "";
+
+    public Object getSMTPConnection() {
+        return null;
+    }
+}
